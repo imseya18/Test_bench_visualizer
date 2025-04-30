abcb6ba1c2251e406f4dab342538c29ea6af76d9
imseya18 <imseya68@gmail.com>
2025-04-30 14:41:33 +0200

    refactor(back): store token in app state

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,21 +1,25 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Manager;
+
+use std::sync::RwLock;
 use tauri_plugin_store::StoreExt;
+#[derive(Default)]
+struct AppState {
+    gitlab_token: String,
+}
+
 #[tauri::command(rename_all = "snake_case")]
-fn set_api_key(token: &str, app: tauri::AppHandle) -> Result<(), String> {
-    let store = app.store("store.json").map_err(|e| e.to_string())?;
-    store.set("GITLAB_TOKEN", token);
-    println!("{:?}", token);
+fn set_api_key(token: &str, state: tauri::State<'_, RwLock<AppState>>) -> Result<(), String> {
+    let mut state_guard = state.write().map_err(|e| e.to_string())?;
+    state_guard.gitlab_token = token.to_string();
     Ok(())
 }
 
 #[tauri::command(rename_all = "snake_case")]
-fn get_api_key(app: tauri::AppHandle) -> Result<String, String> {
-    let store = app.store("store.json").map_err(|e| e.to_string())?;
-    let token = store.get("GITLAB_TOKEN");
-    match token {
-        Some(token) => Ok(token.to_string()),
-        None => Err(String::from("Error GITLAB_TOKEN: not found")),
-    }
+fn get_api_key(state: tauri::State<'_, RwLock<AppState>>) -> Result<String, String> {
+    let token_guard = state.read().unwrap();
+    let gitlab_token = token_guard.gitlab_token.clone();
+    dbg!(&gitlab_token);
+    Ok(gitlab_token)
 }
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -23,6 +27,16 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![set_api_key, get_api_key])
+        .setup(|app| {
+            let store = app.store("store.json").map_err(|e| e.to_string())?;
+            app.manage(RwLock::new(AppState::default()));
+            if let Some(token) = store.get("GITLAB_TOKEN") {
+                let state = app.state::<RwLock<AppState>>();
+                let mut state = state.write().unwrap();
+                state.gitlab_token = token.as_str().ok_or("not a valide token")?.to_string();
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
