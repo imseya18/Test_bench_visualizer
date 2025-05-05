2ee49458d9648c23222b15a350669c4bf4e3ec6a
imseya18 <imseya68@gmail.com>
2025-05-05 10:41:32 +0200

    feat(back): set up store for Api token

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,5 @@
 use tauri::Manager;
-
+use tauri::path::BaseDirectory;
 use std::sync::RwLock;
 use tauri_plugin_store::StoreExt;
 #[derive(Default)]
@@ -8,9 +8,12 @@ struct AppState {
 }
 
 #[tauri::command(rename_all = "snake_case")]
-fn set_api_key(token: &str, state: tauri::State<'_, RwLock<AppState>>) -> Result<(), String> {
+fn set_api_key(token: &str, state: tauri::State<'_, RwLock<AppState>>, app: tauri::AppHandle<>) -> Result<(), String> {
     let mut state_guard = state.write().map_err(|e| e.to_string())?;
     state_guard.gitlab_token = token.to_string();
+    let resource_path = app.path().resolve("json/store.json", BaseDirectory::Resource).map_err(|e| e.to_string())?;
+    let store = app.store(resource_path).map_err(|e| e.to_string())?;
+    store.set("GITLAB_TOKEN", token);
     Ok(())
 }
 
@@ -28,11 +31,12 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![set_api_key, get_api_key])
         .setup(|app| {
-            let store = app.store("store.json").map_err(|e| e.to_string())?;
+            let resource_path = app.path().resolve("json/store.json", BaseDirectory::Resource)?;
+            let store = app.store(resource_path).map_err(|e| e.to_string())?;
             app.manage(RwLock::new(AppState::default()));
             if let Some(token) = store.get("GITLAB_TOKEN") {
                 let state = app.state::<RwLock<AppState>>();
-                let mut state = state.write().unwrap();
+                let mut state: std::sync::RwLockWriteGuard<'_, AppState> = state.write().unwrap();
                 state.gitlab_token = token.as_str().ok_or("not a valide token")?.to_string();
             }
             Ok(())
