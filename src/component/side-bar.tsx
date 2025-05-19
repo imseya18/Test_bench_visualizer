4453c6921edf94e1bd2fae2f428ddb837620e169
imseya18 <imseya68@gmail.com>
2025-05-19 14:16:05 +0200

    feat(front): move sync button to side bar

--- src/component/side-bar.tsx
+++ src/component/side-bar.tsx
@@ -1,9 +1,11 @@
 import { Button, Tooltip, Divider } from '@heroui/react';
 import { Icon } from '@iconify/react';
 import { useNavigate } from 'react-router-dom';
-
+import { useBoardStore } from '../utils/board-store';
 export function Sidebar() {
   const navigate = useNavigate();
+  const fetchGitLabData = useBoardStore((state) => state.fetchGitLabData);
+  const apiLoading = useBoardStore((state) => state.isLoading);
   return (
     <div className='h-screen w-16 bg-content2 flex flex-col items-center py-4 border-r border-content3'>
       <Tooltip content='Dashboard' placement='right'>
@@ -76,7 +78,18 @@ export function Sidebar() {
         </Button>
       </Tooltip>
 
-      <div className='mt-auto'>
+      <div className='mt-auto flex flex-col items-center '>
+        <Tooltip content='Refresh' placement='right'>
+          <Button
+            isIconOnly
+            isLoading={apiLoading ? true : undefined}
+            onPress={fetchGitLabData}
+            className='mb-4'
+            variant='light'
+          >
+            <Icon icon='material-symbols:clock-loader-60-sharp' width={24} />
+          </Button>
+        </Tooltip>
         <Tooltip content='Help' placement='right'>
           <Button isIconOnly variant='light' className='mb-4' aria-label='Help'>
             <Icon icon='lucide:help-circle' width={24} />
